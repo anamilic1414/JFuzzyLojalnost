9034b43d7e32a5a4a5c64f841bc61b5c4c9ddee2
anamilic1414 <ana.milic.pz@gmail.com>
2019-09-14 17:16:02 +0200

    Dodate stvarne vrednosti lojalnosti i uradjena evaluacija.

--- FuzzySistemLojalnosti/src/Proba.java
+++ FuzzySistemLojalnosti/src/Proba.java
@@ -8,7 +8,6 @@ import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Rule;
 import net.sourceforge.jFuzzyLogic.rule.RuleBlock;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
-import neuronske.TestiranjeSvihVarijabli;
 
 import java.io.*;
 import java.sql.*;
@@ -30,7 +29,7 @@ public class Proba {
 		String pravila = new String();
 		for (Rule r : fis.getFunctionBlock("blok").getFuzzyRuleBlock("ruleBlok").getRules())
 			pravila = pravila + "\nPravilo " + r.getName() + ": w=[" + r.getWeight() + "] (" + r.getDegreeOfSupport()
-					+ ") ";// +r.toString();
+					+ ") ";
 
 		return pravila;
 	}
@@ -69,15 +68,15 @@ public class Proba {
 	}
 
 	public void GenerisiTreningPodatke() {
-		
-		//TestiranjePodataka();
+
+		// TestiranjePodataka();
 		UzimanjePodataka();
+
 	}
 
 	public void appendStrToFile(String str) {
 		try {
-			String fileName = "testL.txt";
-// Open given file in append mode. 
+			String fileName = "proba1.txt";
 			BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
 			out.write(str + "\n");
 			out.close();
@@ -105,34 +104,6 @@ public class Proba {
 		return "NA";
 	}
 
-	public void TestiranjePodataka() {
-		RuleBlock ruleBlock = fis.getFunctionBlock(null).getFuzzyRuleBlock(null);
-		// Variable lPotrosnja = ruleBlock.getVariable("potrosnja");
-		// Variable lUcestalost = ruleBlock.getVariable("ucestalost");
-		// Variable lRedosvnost = ruleBlock.getVariable("redovnostPlacanja");
-		// Variable lReklamacije = ruleBlock.getVariable("reklamacije");
-		// Variable lKomunikacija = ruleBlock.getVariable("komunikacija");
-		// Variable lVremenski = ruleBlock.getVariable("vremenskiKupac");
-		// Variable lPreporuke = ruleBlock.getVariable("preporuke");
-		Variable lLojalnost = ruleBlock.getVariable("lojalnost");
-		// Add variables to be optimized to parameter list
-		ArrayList<Parameter> parameterList = new ArrayList<Parameter>();
-		// parameterList.addAll(Parameter.parametersMembershipFunction(lPotrosnja));
-		// parameterList.addAll(Parameter.parametersMembershipFunction(lUcestalost));
-		// parameterList.addAll(Parameter.parametersMembershipFunction(lRedosvnost));
-		// parameterList.addAll(Parameter.parametersMembershipFunction(lReklamacije));
-		// parameterList.addAll(Parameter.parametersMembershipFunction(lKomunikacija));
-		// parameterList.addAll(Parameter.parametersMembershipFunction(lVremenski));
-		// parameterList.addAll(Parameter.parametersMembershipFunction(lPreporuke));
-		parameterList.addAll(Parameter.parametersMembershipFunction(lLojalnost));
-		// Create optimizaion object and run it
-		ErrorFunction errFun = new ErrorFunctionLojalnost("trening.txt");
-		OptimizationDeltaJump optimization = new OptimizationDeltaJump(ruleBlock, errFun, parameterList);
-		optimization.optimize();
-		
-		//JFuzzyChart.get().chart(fis.getVariable("lojalnost"), fis.getVariable("lojalnost").getDefuzzifier(), true);
-	}
-
 	public void UzimanjePodataka() {
 		// pravljenje konekcije
 		String konekcioniString = "jdbc:mysql://localhost/kupci?user=root";
@@ -142,29 +113,22 @@ public class Proba {
 		Statement stmt = null;
 		ResultSet rs = null;
 
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(konekcioniString);
 
-			String SQL = "SELECT * FROM kupci.kupcisvi";
+			String SQL = "SELECT * FROM kupci.kupcinovi";
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(SQL);
-
 			while (rs.next()) {
-				if (rs.getInt(1) > 923) {
-					SetovanjeVrednosti(rs.getDouble(3), rs.getInt(4), rs.getDouble(5), rs.getDouble(6),
-							rs.getInt(7), rs.getInt(8), rs.getInt(9));
-					// System.out.println(""+rs.getInt(2)+","+fis.getVariable("lojalnost").getValue());
+				
+					SetovanjeVrednosti(rs.getDouble(3), rs.getInt(4), rs.getDouble(5), rs.getDouble(6), rs.getInt(7),
+							rs.getInt(8), rs.getInt(9));
 
-					// System.out.println(vratiSkup(fis.getVariable("lojalnost").getValue()));
-					appendStrToFile(/*rs.getDouble(3) + "," + rs.getInt(4) + "," + rs.getDouble(5) + ","
-							+ rs.getDouble(6) + "," + rs.getInt(7) + "," + rs.getInt(9) + "," + rs.getInt(8)
-							+ "," +*/ fis.getVariable("lojalnost").getValue()+"," + vratiSkup(fis.getVariable("lojalnost").getValue()));
-
-					// System.out.println(fis.getVariable("lojalnost").getValue());
+					appendStrToFile(vratiSkup(fis.getVariable("lojalnost").getValue()) + "," + rs.getString(10));
+					
 				}
-
-			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
